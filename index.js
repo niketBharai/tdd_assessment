3262645cacd9bbc6d770adc41a873588e7c22edb
Niket <niksbharai.1@gmail.com>
2024-10-01 14:30:35 +0530

    feat: support for different delimiters

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ class StringCalculator {
     if (numbers === "") return 0;
 
     let delimiter = /[,\n]/;
+    let tempNum = numbers;
 
-    const numArray = numbers.split(delimiter).map(Number);
+    if (numbers.startsWith("//")) {
+      const numWithDelimiter = numbers.split("\n");
+      delimiter = new RegExp(numWithDelimiter[0].substring(2));
+      tempNum = numWithDelimiter.slice(1).join("\n");
+    }
+
+    const numArray = tempNum.split(delimiter).map(Number);
 
     return numArray.reduce((sum, num) => sum + num, 0);
   }
@@ -15,3 +22,5 @@ const calculator = new StringCalculator();
 console.log(calculator.add(""));
 console.log(calculator.add("1"));
 console.log(calculator.add("1,5"));
+console.log(calculator.add("1\n2,3"));
+console.log(calculator.add("//;\n1;2"));
