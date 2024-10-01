8353550b82d53f752a83a31c9aefe385c4830b35
Niket <niksbharai.1@gmail.com>
2024-10-01 14:16:44 +0530

    feat: check for multiple numbers

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 class StringCalculator {
   add(numbers) {
     if (numbers === "") return 0;
+
+    let delimiter = /[,\n]/;
+
+    const numArray = numbers.split(delimiter).map(Number);
+
+    return numArray.reduce((sum, num) => sum + num, 0);
   }
 }
 
 const calculator = new StringCalculator();
 
 console.log(calculator.add(""));
+console.log(calculator.add("1"));
+console.log(calculator.add("1,5"));
